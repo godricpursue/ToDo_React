11da27a19fcfa44981fabf7f35ffd72fd7bf8d60
Altan Simsir <altansimsir@outlook.com>
2022-10-03 15:32:24 +0300

    Initial Files

---
+++ src/components/ToDoItem.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function ToDoItem(props) {
+  return (
+    <div
+      onClick={() => {
+        props.onChecked(props.id);
+      }}
+    >
+      <li
+        style={{
+          cursor: "pointer"
+        }}
+      >
+        {props.text}
+      </li>
+    </div>
+  );
+}
+
+export default ToDoItem;
